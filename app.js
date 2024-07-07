69933018388f53f1a982407feccb3309f5764578
Денис <tolmachevdenistolmach@gmail.com>
2024-07-07 20:34:46 +1000

    Бот имеет 3 команды

--- app.js
+++ app.js
@@ -4,8 +4,41 @@ const token = '7412518611:AAE_dnjqxx_EwljH6Inch6xRBBrs090u8ZM';
 
 const bot = new TelegramBot(token, {polling: true});
 
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
+const keyboard = [
+  [{ text: '/help' }],
+  [{ text: '/site' }],
+  [{ text: '/creator' }],
+];
 
-  bot.sendMessage(chatId, 'Привет, октагон!');
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Доброго времени суток! Выберите команду:', {
+    reply_markup: {
+      keyboard: keyboard,
+      resize_keyboard: true,
+      one_time_keyboard: true,
+    },
+  });
+});
+
+bot.onText(/\/help/, (msg) => {
+  const chatId = msg.chat.id;
+  const helpMessage = `
+  Список доступных команд:
+  /site - ссылка на сайт октагона
+  /creator - отправляет в чат ФИО
+  `;
+  bot.sendMessage(chatId, helpMessage);
+});
+
+bot.onText(/\/site/, (msg) => {
+  const chatId = msg.chat.id;
+  const siteUrl = 'https://students.forus.ru/';
+  bot.sendMessage(chatId, `Сайт октагона: ${siteUrl}`);
+});
+
+bot.onText(/\/creator/, (msg) => {
+  const chatId = msg.chat.id;
+  const creatorInfo = 'Толмачев Денис Андреевич';
+  bot.sendMessage(chatId, `Разработал: ${creatorInfo}`);
 });
